a4a5a37bea7bfee9bb2b904f467bd334a3d6d9c6
ScriptRUs <69376579+ScriptRUs@users.noreply.github.com>
2022-03-11 00:40:39 -0600

    Update NinjaStarSimulator.lua

--- NinjaStarSimulator.lua
+++ NinjaStarSimulator.lua
@@ -2,6 +2,11 @@
 Scripting By: Ryans#3857
 Cash App: $ScriptRUs
 ]]--
+if _G.Toggle == true then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-302.18402099609375, 954.5215454101562, -462.1951904296875)
+elseif _G.Toggle == false then
+    wait()
+end
 while _G.Toggle do wait(2)
 if _G.Toggle == true then
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
@@ -12,10 +17,11 @@ for i,v in pairs(game:GetService("Workspace").MonsterSpawners.Japan[_G.Map]:GetD
         tween:Play()
     end
 end
-        elseif _G.Toggle == false then
+elseif _G.Toggle == false then
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
 end
 end
+
 while _G.AutoCollect do wait(0)
 for _,v in pairs(game:GetService("Workspace").Coin:GetDescendants()) do
     if v:IsA("TouchTransmitter") then
@@ -23,4 +29,5 @@ for _,v in pairs(game:GetService("Workspace").Coin:GetDescendants()) do
         wait()
         firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v.Parent, 1) -- 1 is untouch
     end
+end
 end
